79999e56c1611902b65018eb62adedc759cfe826
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-07-07 10:47:58 +0930

    Initial Commit

---
+++ KnittingProjects/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KnittingProjects
+//
+//  Created by Rebecca Cordingley on 7/07/2014.
+//  Copyright (c) 2014 Rebecca Cordingley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
